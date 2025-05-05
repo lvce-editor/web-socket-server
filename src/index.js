2111ff857796defbfe05b1fd1a2080d4ce010193
Le Vivilet <levivlet@pm.me>
2025-05-05 23:53:36 +0200

    feature: use native promise.withResolvers function

--- src/index.js
+++ src/index.js
@@ -14,31 +14,8 @@ const webSocketServer = new WebSocketServer({
   // perMessageDeflate: true
 })
 
-const withResolvers = () => {
-  /**
-   * @type {any}
-   */
-  let _resolve
-  /**
-   * @type {any}
-   */
-  let _reject
-  const promise = new Promise((resolve, reject) => {
-    _resolve = resolve
-    _reject = reject
-  })
-  return {
-    resolve: _resolve,
-    reject: _reject,
-    promise,
-  }
-}
-
 export const handleUpgrade = (request, socket) => {
-  const { promise, resolve } = withResolvers()
-  const upgradeCallback = (ws) => {
-    resolve(ws)
-  }
-  webSocketServer.handleUpgrade(request, socket, Buffer.alloc(0), upgradeCallback)
+  const { promise, resolve } = Promise.withResolvers()
+  webSocketServer.handleUpgrade(request, socket, Buffer.alloc(0), resolve)
   return promise
 }
