356c27a75d3073c2daf544eda8551b3e867717b9
Le Vivilet <levivlet@pm.me>
2025-05-06 00:33:17 +0200

    feature: lazyload ws to work better with bundling for electron where ws is not used

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import { Buffer } from 'node:buffer'
-import * as _ws from 'ws'
 
-// workaround for jest or node bug
-const WebSocketServer = _ws.WebSocketServer
-  ? _ws.WebSocketServer
-  : // @ts-ignore
-    _ws.default.WebSocketServer
+const createWebSocketServer = async () => {
+  const _ws = await import('ws')
+  // workaround for jest or node bug
+  const WebSocketServer = _ws.WebSocketServer
+    ? _ws.WebSocketServer
+    : // @ts-ignore
+      _ws.default.WebSocketServer
 
-const webSocketServer = new WebSocketServer({
-  noServer: true,
+  const webSocketServer = new WebSocketServer({
+    noServer: true,
 
-  // TODO not sure if ws compress is working at all
-  // perMessageDeflate: true
-})
+    // TODO not sure if ws compress is working at all
+    // perMessageDeflate: true
+  })
+  return webSocketServer
+}
 
-export const handleUpgrade = (request, socket) => {
+const doSocketUpgrade = (webSocketServer, request, socket) => {
   const { promise, resolve } = Promise.withResolvers()
   webSocketServer.handleUpgrade(request, socket, Buffer.alloc(0), resolve)
   return promise
+}
+
+export const handleUpgrade = async (request, socket) => {
+  const webSocketServer = await createWebSocketServer()
+  const webSocket = await doSocketUpgrade(webSocketServer, request, socket)
+  return webSocket
 }
